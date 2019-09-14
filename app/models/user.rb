75192c1a758f121a0fb11131a8ac05abf3d44273
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-09-14 23:22:07 +0900

    setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
+  mount_uploader :photo, PhotoUploader
   has_many :interests
   has_many :interested_events, :through => :interests, :source => :event
 
